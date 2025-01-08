a236cba21913060b8e151aa24e945ec0facfb3d9
tz <tztw4723@gmail.com>
2025-01-09 00:18:47 +0800

    add account route

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+
+import Account from "@/db/account.model";
+import handleError from "@/lib/handlers/error";
+import { ForbiddenError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+
+export async function GET() {
+  try {
+    await dbConnect();
+    const account = await Account.find();
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+    const validatedData = AccountSchema.parse(body);
+
+    const existingAccount = await Account.findOne({
+      provider: validatedData.provider,
+      providerAccountId: validatedData.providerAccountId,
+    });
+    if (existingAccount) throw new ForbiddenError("Account already exists");
+
+    const newAccount = await Account.create(validatedData);
+    return NextResponse.json(
+      { success: true, data: newAccount },
+      { status: 201 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
